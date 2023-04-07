bfed3f63474e8d8d880c7e915a4095f8dc9d4b0b
neekesh <nikesthapa20@gmail.com>
2023-04-07 09:15:59 +0545

    feat: login page with no firebase

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,66 +1,151 @@
  
 import { signInWithEmailAndPassword } from "firebase/auth"
-import { auth } from "@/context/firebaseAuth/firebaseConfig"
-import { useState } from "react"
-import { useRouter } from 'next/router'
+import { auth, database } from "@/context/firebaseAuth/firebaseConfig"
+import { useState, useEffect } from "react"
+import { LockOutlined, UserOutlined } from '@ant-design/icons';
+import { Button, Form, Input, Card } from 'antd';
+import { useRouter } from 'next/router';
+import styled from "styled-components";
+import Image from 'next/image'
+
+
+const LoginCard = styled.div`
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    .card {
+
+      height: 75vh;
+      width: 75vw;
+      /* border: solid; */
+      background-color: white;
+      border-radius: 0.5rem;
+      box-shadow:0 1rem 3rem rgba(0,0,0,.175)!important
+    }
+    .login-wrapper{
+      display: flex;
+      justify-content: center;
+    }
+    .form {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      padding: 2rem;
+      width: 100%;
+      height: 100%:
+    }
+    .image {
+      width: 100%;
+      height: 100%;
+      position: relative;
+    }
+    .image > img {
+      border-radius: 0 0.5rem 0.5rem 0;
+    }
+    input {
+      width: 20rem;
+    }
+    .heading {
+      font-size: 3rem;
+      margin-bottom: 1rem;
+    }
+    button {
+      width: 100%;
+    }
+`
+
 
 
 function Login(){
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
+  const [form] = Form.useForm()
+  const [, forceUpdate] = useState({});
 
-  
+  useEffect(() => {
+    forceUpdate({});
+  }, []);
+
   const router = useRouter()
-    const handleLogin = async (event) => {
-      event?.preventDefault();
-      try {
-        const data = await signInWithEmailAndPassword(
-          auth,
-          email,
-          password
-        )
-        console.log("data",data)
-        if (!data || !data.user || !auth.currentUser) {
-          return
-        }
-        router.push("/")
-
-      } catch (error) {
-        alert("Invalid Credentials login failed")
-        console.log(error)
+  const onFinish = (values: any) => {
+    try {
+      const data =  signInWithEmailAndPassword(
+        auth,
+        values.email,
+        values.password
+      )
+      console.log("data",data)
+      if (!data  || !auth.currentUser) {
+        console.log("am i here")
+        return
       }
+      router.push("/")
+      // set(ref(database, "admins/" + ))
+      // set(ref(database, "admins/" , {
+      //   "email": values.email,
+      //   "logged-in": true,
+      // }))
+      const adminInfo = database.ref('admins').push()
+       adminInfo.set({
+        "email": values.email,
+        "logged-in": true,
+       })
+
+    } catch (error) {
+      alert("Invalid Credentials login failed")
+      console.log(error)
     }
+  };
+  
     
   return (
-      <>
-      <main className="vh-100 vw-100 d-flex justify-content-center align-items-center" >
-        <div className="card  w-50 h-50">
-          <form className="p-5 ">
-            <h1 className="h3 mb-3 text-center">Please sign in</h1>
-  
-            <div className="form-group p-2">
-              <label htmlFor="email">Email: </label>
-              <input type="email" className="form-control" id="groupInput" placeholder="Enter your email" required  onChange={(event) => setEmail(event.target.value)}/>
-            </div>
-            <div className="form-group p-2">
-              <label htmlFor="password"> Password: </label>
-              <input type="password" className="form-control" id="groupPassword" placeholder="Password" required onChange={(event) => setPassword(event.target.value)} />
-            </div>
-  
-            {/* <div className="mb-3">
-              <label>
-                <input type="checkbox" value="remember-me" /> Remember me
-              </label>
-            </div> */}
-            <div className=" d-flex justify-content-center ">
-
-              <button className="w-50 btn btn-lg btn-primary " type="submit" onClick={handleLogin} style={{backgroundColor:'rgb(27, 99, 53)'}} >Log In </button>
-            </div>
-          </form>
+    <LoginCard>
+      <div className="card login-wrapper">
+        <div className="form">
+          <h1 className="heading">Sign In</h1>
+          <Form form={form} name="horizontal_login" layout="vertical" onFinish={onFinish}>
+          <Form.Item
+            name="email"
+            label="Email"
+            rules={[{ required: true, message: 'Please enter your email here!' }]}
+          >
+            <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="email" />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            label="Password"
+            rules={[{ required: true, message: 'Please input your password!' }]}
+          >
+            <Input
+              prefix={<LockOutlined className="site-form-item-icon" />}
+              type="password"
+              placeholder="Password"
+            />
+          </Form.Item>
+          <Form.Item shouldUpdate>
+            {() => (
+              <Button
+                type="primary"
+                htmlType="submit"
+                style={{ background: "#F04242", borderColor: "yellow" }}
+                disabled={
+                  !form.isFieldsTouched(true) ||
+                  !!form.getFieldsError().filter(({ errors }) => errors.length).length
+                }
+              >
+                Log in
+              </Button>
+            )}
+          </Form.Item>
+          </Form>
         </div>
-      </main>
-
-    </>
+        <div className="image">
+          <Image src="/splash_image.jpg" alt="My Image" fill />
+        </div>
+      </div>
+    </LoginCard>
   )
 }
 
